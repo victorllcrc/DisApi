3f9472eabcf60aa223657d040ec0bd55a3bbbb2d
unknown <victor.llancari@unmsm.edu.pe>
2023-08-29 12:50:20 -0500

    Fix env

--- server/database/connection.js
+++ server/database/connection.js
@@ -4,9 +4,7 @@ require('dotenv').config();
 const connectDB = async () => {
     try{
         // mongodb connection string
-        const con = await mongoose.connect(
-        "mongodb://admin:adminsisdis@ac-zfxs25s-shard-00-00.spkzqnf.mongodb.net:27017,ac-zfxs25s-shard-00-01.spkzqnf.mongodb.net:27017,ac-zfxs25s-shard-00-02.spkzqnf.mongodb.net:27017/?ssl=true&replicaSet=atlas-xpodvc-shard-0&authSource=admin&retryWrites=true&w=majority", 
-        {
+        const con = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
